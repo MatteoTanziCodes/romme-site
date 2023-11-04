d93fe0c67ec191a7393253345b67faf41d13caec
Matteo Tanzi <matteo.beatstanzi@gmail.com>
2023-11-04 02:30:18 -0400

    updates

--- src/components/main.tsx
+++ src/components/main.tsx
@@ -5,7 +5,7 @@ const Main = () => {
   return (
     <div className='main'>
       <div className="bg-video">
-        <video className="bg-video__content" autoPlay muted loop src={`/vid.mp4`} />
+        <video className="bg-video__content" autoPlay playsInline muted loop src={`/vid.mp4`} />
       </div>
       <div className='content'>
         <img className="overlay" src={`/rmcs.png`} />
